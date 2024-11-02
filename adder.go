367cc36b67e4fe87bef4009968ef499c7370f74e
Rizvan <rizvan.chalilovas@proton.me>
2024-11-02 15:45:49 +0200

    Add gogoc

---
+++ adder.go
@@ -0,0 +1,7 @@
+// Solution for excercises in chaper 10 of book Learning Go, 2nd edition.
+package Adder
+
+// Add is a function that takes two integers and adds them together, returning their sum.
+func Add(i1, i2 int) int {
+	return i1 + i2
+}
--- main.go
+++
@@ -1,5 +0,0 @@
-package Adder
-
-func Add(i1, i2 int) int {
-	return i1 + i2
-}
