9a08a067bd5e445a3ad757d835247bd6b9aa8c66
Rizvan <rizvan.chalilovas@proton.me>
2024-11-02 16:27:31 +0200

    Change Add functoin to use generics

--- adder.go
+++ adder.go
@@ -1,7 +1,15 @@
 // Solution for excercises in chaper 10 of book Learning Go, 2nd edition.
 package Adder
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add is a function that takes two integers and adds them together, returning their sum.
-func Add(i1, i2 int) int {
-	return i1 + i2
+func Add[T Number](n1, n2 T) T {
+	return n1 + n2
 }
